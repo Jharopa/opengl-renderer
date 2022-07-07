1f06a505f31602820567b135f401c9a67a00bafd
Alex Burns <jharopa@gmail.com>
2022-07-07 18:17:37 +0100

    Dot and Cross Product

--- source/engine/main.cpp
+++ source/engine/main.cpp
@@ -1,11 +1,20 @@
 #include "application.h"
 #include "vec.h"
+#include <type_traits>
 
 int main()
 {
-    vec2 v = vec2();
+    math::vec3 v1 = math::vec3(2, 3, 4);
+    math::vec3 v2 = math::vec3(5, 6, 7);
 
-    std::cout << v.x << " " << v.y << std::endl;
+    f32 d = math::dot(v1, v2);
+    math::vec3 cv = math::cross(v1, v2);
+
+    std::cout << d << std::endl;
+    std::cout << "(" << cv.x << ", " << cv.y << ", " << cv.z << ")" << std::endl;
+
+    std::cout << std::is_trivial<math::vec2>() << std::endl;
+    std::cout << std::is_standard_layout<math::vec2>() << std::endl;
 
     Application::getInstance().init();
 
